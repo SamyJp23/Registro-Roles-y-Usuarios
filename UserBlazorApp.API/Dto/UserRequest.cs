f73d1c5d99062ffb988a08d9bf160a1ba510ed9e
samueljavier880@gmail.com <samueljavier880@gmail.com>
2024-07-03 23:57:30 -0400

    Agregando los Dto para usuarios

---
+++ UserBlazorApp.API/Dto/UserRequest.cs
@@ -0,0 +1,13 @@
+﻿namespace UserBlazorApp.API.Dto
+{
+    public class UserRequest
+    {
+        
+            public string? UserName { get; set; }
+            public string? Email { get; set; }
+            public string? PhoneNumber { get; set; }
+         
+        
+
+    }
+}
