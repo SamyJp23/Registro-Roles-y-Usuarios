4b1adaaa0112f4eee1e6656e0bcbf94fd8c9d803
samueljavier880@gmail.com <samueljavier880@gmail.com>
2024-07-03 23:58:51 -0400

    Creando las interfaces

---
+++ UsersBlazorApp.Data/Interfaces/UsersInterface.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsersBlazorApp.Data.Interfaces;
+
+public interface UsersInterface<U>
+{
+    Task<List<U>> GetAllAsync();
+    Task<U> GetByIdAsync(int id);
+    Task<U> AddAsync(U entity);
+    Task<bool> UpdateAsync(U entity);
+    Task<bool> DeleteAsync(int id);
+}
