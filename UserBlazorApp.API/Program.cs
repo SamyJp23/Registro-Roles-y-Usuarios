202ac178b1b1a5804a574b2ce1c080d3e9d49f0c
samueljavier880@gmail.com <samueljavier880@gmail.com>
2024-07-04 00:08:45 -0400

    Configurando los Cors e Inyectando el service

--- UserBlazorApp.API/Program.cs
+++ UserBlazorApp.API/Program.cs
@@ -1,6 +1,9 @@
 
 using Microsoft.EntityFrameworkCore;
-using UsersBlazorApp.Data.Context;
+using UserBlazorApp.API.Context;
+using UserBlazorApp.API.Services;
+using UsersBlazorApp.Data.Interfaces;
+using UsersBlazorApp.Data.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,8 +15,19 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<UsersDbContext>(o => o.UseSqlServer(builder.Configuration.GetConnectionString("ConStr")));
+builder.Services.AddScoped<ApiInterface<AspNetUsers>, UsersServices>();
 
+//Cors
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", builder =>
+    {
+        builder.AllowAnyOrigin()
+               .AllowAnyMethod()
+               .AllowAnyHeader();
+    });
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -25,8 +39,10 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.UseAuthorization();
-
 app.MapControllers();
+
+app.UseCors("AllowAll");
+
+app.UseAuthorization();
 
 app.Run();
