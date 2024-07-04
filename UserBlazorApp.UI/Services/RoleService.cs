26e4b886f27cbde6eb56d9954bbb625d52ab8a34
samueljavier880@gmail.com <samueljavier880@gmail.com>
2024-07-04 02:42:30 -0400

    Creando los servicios para roles y UserClaims

---
+++ UserBlazorApp.UI/Services/RoleService.cs
@@ -0,0 +1,42 @@
+﻿using System.Net.Http.Json;
+using UsersBlazorApp.Data.Interfaces;
+using UsersBlazorApp.Data.Models;
+
+namespace UserBlazorApp.UI.Services;
+
+public class RoleService : UsersInterface<AspNetRoles>
+{
+    private readonly HttpClient _httpClient;
+
+    public RoleService(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+    public async Task<List<AspNetRoles>> GetAllAsync()
+    {
+        return (await _httpClient.GetFromJsonAsync<List<AspNetRoles>>("https://localhost:7097/api/Roles"))!;
+    }
+
+    public async Task<AspNetRoles> GetByIdAsync(int id)
+    {
+        return (await _httpClient.GetFromJsonAsync<AspNetRoles>($"api/Users/{id}"))!;
+    }
+
+    public async Task<AspNetRoles> AddAsync(AspNetRoles entity)
+    {
+        var response = await _httpClient.PostAsJsonAsync("https://localhost:7097/api/Roles", entity);
+        return (await response.Content.ReadFromJsonAsync<AspNetRoles>())!;
+    }
+
+    public async Task<bool> UpdateAsync(AspNetRoles entity)
+    {
+        var response = await _httpClient.PutAsJsonAsync($"api/Users/{entity.Id}", entity);
+        return response.IsSuccessStatusCode;
+    }
+
+    public async Task<bool> DeleteAsync(int id)
+    {
+        var response = await _httpClient.DeleteAsync($"api/Users/{id}");
+        return response.IsSuccessStatusCode;
+    }
+}
