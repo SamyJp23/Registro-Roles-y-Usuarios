ffcbbaeb7dfe5a659a8e429773a097a1da50d269
samueljavier880@gmail.com <samueljavier880@gmail.com>
2024-07-03 23:59:40 -0400

    Creando el servicio para la UI

---
+++ UserBlazorApp.UI/Services/UserService.cs
@@ -0,0 +1,42 @@
+﻿using System.Net.Http.Json;
+using UsersBlazorApp.Data.Interfaces;
+using UsersBlazorApp.Data.Models;
+
+namespace UserBlazorApp.UI.Services;
+
+public class UserService : UsersInterface<AspNetUsers>
+{
+    private readonly HttpClient _httpClient;
+
+    public UserService(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+    public async Task<List<AspNetUsers>> GetAllAsync()
+    {
+        return (await _httpClient.GetFromJsonAsync<List<AspNetUsers>>("https://localhost:7097/api/Users"))!;
+    }
+
+    public async Task<AspNetUsers> GetByIdAsync(int id)
+    {
+        return (await _httpClient.GetFromJsonAsync<AspNetUsers>($"api/Users/{id}"))!;
+    }
+
+    public async Task<AspNetUsers> AddAsync(AspNetUsers entity)
+    {
+        var response = await _httpClient.PostAsJsonAsync("https://localhost:7097/api/Users", entity);
+        return (await response.Content.ReadFromJsonAsync<AspNetUsers>())!;
+    }
+
+    public async Task<bool> UpdateAsync(AspNetUsers entity)
+    {
+        var response = await _httpClient.PutAsJsonAsync($"api/Users/{entity.Id}", entity);
+        return response.IsSuccessStatusCode;
+    }
+
+    public async Task<bool> DeleteAsync(int id)
+    {
+        var response = await _httpClient.DeleteAsync($"api/Users/{id}");
+        return response.IsSuccessStatusCode;
+    }
+}
